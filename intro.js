51c1307e3ba981d5f7ea14afed1b48f9ba49c408
AjithAmin004 <ajithsowda33@gmail.com>
2022-03-05 21:50:59 +0530

    Variable introduction

--- intro.js
+++ intro.js
@@ -1,1 +1,13 @@
 console.log("Hai Ajith");
+
+let num = 10;
+console.log(num);
+
+let char = 'a';
+console.log(char);
+
+let str = "I am Ajith Amin";
+console.log(str);
+
+let bool =(true);
+console.log(bool);
