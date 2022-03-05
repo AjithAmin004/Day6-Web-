d8dee57a73f9d1bf28a28407b48a556495ea01c2
AjithAmin004 <ajithsowda33@gmail.com>
2022-03-06 00:00:20 +0530

    String and its some method

--- array.js
+++ array.js
@@ -4,6 +4,7 @@ console.log(arr)
 
 arr[0]="Hai";
 console.log(arr)
-
+//1 . push
 arr.push("add last")//this will add 1 more at the end of array
 console.log(arr)
+//2 . pop
---
+++ string.js
@@ -0,0 +1,10 @@
+let str = "I am Ajith";
+
+console.log(str.length);
+
+console.log(str.slice(2,6));
+console.log(str.replace("am","was"));
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.charAt(1));
+console.log(str.split(" "));
